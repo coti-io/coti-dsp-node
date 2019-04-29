74b908179d970a7db23455f25274106962c15f8b
cotitech <eli@coti.io>
2019-04-29 15:49:02 +0300

    Private MainNet is up

--- dspnode/src/main/java/io/coti/dspnode/services/InitializationService.java
+++ dspnode/src/main/java/io/coti/dspnode/services/InitializationService.java
@@ -1,54 +1,88 @@
 package io.coti.dspnode.services;
 
-import io.coti.basenode.data.AddressData;
-import io.coti.basenode.data.NodeType;
-import io.coti.basenode.data.TransactionData;
+import io.coti.basenode.communication.interfaces.IPropagationPublisher;
+import io.coti.basenode.crypto.NodeCryptoHelper;
+import io.coti.basenode.data.*;
+import io.coti.basenode.data.interfaces.IPropagatable;
 import io.coti.basenode.services.BaseNodeInitializationService;
-import io.coti.basenode.services.CommunicationService;
+import io.coti.basenode.services.interfaces.ICommunicationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Service
-public class InitializationService {
+public class InitializationService extends BaseNodeInitializationService {
     @Value("${receiving.port}")
     private String receivingPort;
-    @Value("#{'${propagation.server.addresses}'.split(',')}")
-    private List<String> propagationServerAddresses;
     @Value("${propagation.port}")
     private String propagationPort;
-    @Value("#{'${zerospend.receiving.address}'.split(',')}")
-    private List<String> receivingServerAddresses;
-
-    @Autowired
-    private BaseNodeInitializationService baseNodeInitializationService;
+    @Value("${server.port}")
+    private String serverPort;
     @Autowired
     private TransactionService transactionService;
     @Autowired
     private AddressService addressService;
     @Autowired
-    private CommunicationService communicationService;
+    private ICommunicationService communicationService;
+    @Autowired
+    private IPropagationPublisher propagationPublisher;
+    private EnumMap<NodeType, List<Class<? extends IPropagatable>>> publisherNodeTypeToMessageTypesMap = new EnumMap<>(NodeType.class);
 
     @PostConstruct
     public void init() {
+        super.initDB();
+        super.createNetworkNodeData();
+        super.getNetwork();
 
+        publisherNodeTypeToMessageTypesMap.put(NodeType.ZeroSpendServer, Arrays.asList(TransactionData.class, DspConsensusResult.class));
+        publisherNodeTypeToMessageTypesMap.put(NodeType.FinancialServer, Arrays.asList(TransactionData.class));
+
+        communicationService.initSubscriber(NodeType.DspNode, publisherNodeTypeToMessageTypesMap);
+        NetworkNodeData zerospendNetworkNodeData = networkService.getSingleNodeData(NodeType.ZeroSpendServer);
+        if (zerospendNetworkNodeData == null) {
+            log.error("No zerospend server exists in the network got from the node manager, about to exit application");
+            System.exit(-1);
+        }
+        networkService.setRecoveryServerAddress(zerospendNetworkNodeData.getHttpFullAddress());
+        communicationService.initPublisher(propagationPort, NodeType.DspNode);
         HashMap<String, Consumer<Object>> classNameToReceiverHandlerMapping = new HashMap<>();
         classNameToReceiverHandlerMapping.put(TransactionData.class.getName(), data ->
                 transactionService.handleNewTransactionFromFullNode((TransactionData) data));
+
         classNameToReceiverHandlerMapping.put(AddressData.class.getName(), data ->
                 addressService.handleNewAddressFromFullNode((AddressData) data));
 
         communicationService.initReceiver(receivingPort, classNameToReceiverHandlerMapping);
-        communicationService.initSender(receivingServerAddresses);
-        communicationService.initSubscriber(propagationServerAddresses, NodeType.DspNode);
-        communicationService.initPropagator(propagationPort);
+        communicationService.addSender(zerospendNetworkNodeData.getReceivingFullAddress());
+        communicationService.addSubscription(zerospendNetworkNodeData.getPropagationFullAddress(), NodeType.ZeroSpendServer);
+        List<NetworkNodeData> dspNetworkNodeDataList = networkService.getMapFromFactory(NodeType.DspNode).values().stream()
+                .filter(dspNode -> !dspNode.equals(networkService.getNetworkNodeData()))
+                .collect(Collectors.toList());
+        networkService.addListToSubscription(dspNetworkNodeDataList);
 
-        baseNodeInitializationService.init();
+        super.init();
+    }
 
+    @Override
+    protected NetworkNodeData createNodeProperties() {
+        NetworkNodeData networkNodeData = new NetworkNodeData(NodeType.DspNode, nodeIp, serverPort, NodeCryptoHelper.getNodeHash(), networkType);
+        networkNodeData.setPropagationPort(propagationPort);
+        networkNodeData.setReceivingPort(receivingPort);
+        return networkNodeData;
+    }
+
+    @Override
+    protected void propagateMissingTransaction(TransactionData transactionData) {
+        propagationPublisher.propagate(transactionData, Arrays.asList(NodeType.FullNode));
     }
 }
